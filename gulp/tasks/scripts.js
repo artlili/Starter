284c9d10fd6d189a4c2b3bd491ea3eb44c6cfc86
Lili <mishel-dark@mail.ru>
2018-10-20 23:44:06 +0300

    update gulp

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,8 +2,7 @@ module.exports = function() {
   $.gulp.task('scripts:libs', function() {
     return $.gulp.src([
       'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/slick-carousel/slick/slick.min.js',
-      'node_modules/bootstrap/dist/js/bootstrap.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js'
     ])
     .pipe($.gp.concat('libs.min.js'))
     .pipe($.gulp.dest('build/js'))
